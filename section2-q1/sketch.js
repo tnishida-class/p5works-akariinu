79840ac6d775edb0a777692988b9ae6bcc1f1864
akariinu <akari.n.kughs@gmail.com>
2020-10-20 13:46:00 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,18 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
+  createCanvas(110,110);
+  background(255);
+  noFill();
+  for(let i = 0; i < 50; i++){
     // BLANK[1]
+  for(let s = 10; s <= 110; s = s + 10){
+  if(s <= 60){
+  stroke(0, 0, 255);
+}
+else{
+  stroke(255, 0, 0);
+}
+ellipse(50, 50, s);
   }
+}
 }
