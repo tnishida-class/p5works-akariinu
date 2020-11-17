a401114cd6f37a0764b2060da02fc8af1c2dd8ff
akariinu <akari.n.kughs@gmail.com>
2020-11-17 12:20:35 +0900

    section4-2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -5,14 +5,20 @@ function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
-  vx = 8;
-  vy = 8;
+  vx = 6;
+  vy = 6;
+  angle = 0;
 }
 
 function draw(){
   background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
-
+  noStroke();
+  fill(255, 215, 0);
+  star(x, y, 20, angle);
+  angle += 0.1;
+  x += vx;
+  y += vy;
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
   else if(x < 0){ x = width; }
@@ -23,7 +29,7 @@ function draw(){
 function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI + angle;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
