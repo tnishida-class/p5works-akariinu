5021bab7b374ace1c7e1efd1fc5546e23798ea86
akariinu <akari.n.kughs@gmail.com>
2020-11-17 12:05:05 +0900

    section4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,16 +1,24 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
   cycle = 100;
+  size >= 50
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
-  // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  count = (count + 2) % cycle;
+   let size;
+   if (count < 30){
+     size = 100 - count
+   }
+    size = count;
+    noStroke();
+    fill(255, 0, 0);
+  ellipse(width / 2, height / 2, size)
 }
